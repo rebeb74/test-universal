ccc5b0fdce6b178ee51030e52ddb89d35e8583bd
Bertrand PETIT <bertrandpetit10@gmail.com>
2021-04-21 21:31:25 +0200

    test universal

--- src/app/pages.resolver.ts
+++ src/app/pages.resolver.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import {
-  Resolve,
-} from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { map, mergeMap, take, tap } from 'rxjs/operators';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +13,6 @@ export class PagesResolver implements Resolve<any> {
     private db: AngularFireDatabase
   ) { }
 
-  // async resolve(): Promise<Observable<any>> {
-  //   await this.pageDbService.data$.pipe(take(1)).toPromise();
-  //   return this.pageDbService.data$;
-  // }
   resolve(): Observable<any> {
     return this.db.list('/pages').snapshotChanges().pipe(
       map((uploadData) => {
@@ -31,14 +24,4 @@ export class PagesResolver implements Resolve<any> {
     );
   }
 
-  // resolve(): Observable<Page[]> {
-  //   return this.pageDbService.getPages().snapshotChanges().pipe(
-  //     map((pages) => {
-  //       const newPages = pages.map(c => ({ key: c.payload.key, ...c.payload.val() as any }));
-  //       this.pagesService.setPages(newPages)
-  //       return newPages;
-  //     }),
-  //     take(1)
-  //   );
-  // }
 }
